3c52492d55217dc651d10e75d12614ed5922161e
jack-20000 <102782267+jack-20000@users.noreply.github.com>
2022-04-06 08:38:14 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "ffde268a-f07a-4d2a-977e-86dc9d68d73b",
                         "alterId": 0
                     }
                 ],
